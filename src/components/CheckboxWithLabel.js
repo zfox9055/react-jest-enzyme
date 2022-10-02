2f7bb13bb65879adb9f88da146002ade224051c8
Nemanja <nenadv990@gmail.com>
2022-10-02 20:19:32 +0200

    cbox

---
+++ src/components/CheckboxWithLabel.js
@@ -0,0 +1,17 @@
+import {useState} from 'react';
+import React, { Component } from 'react'
+
+export default function CheckboxWithLabel({labelOn, labelOff}) {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const onChange = () => {
+    setIsChecked(!isChecked);
+  };
+
+  return (
+    <label>
+      <input type="checkbox" checked={isChecked} onChange={onChange} />
+      {isChecked ? labelOn : labelOff}
+    </label>
+  );
+}
